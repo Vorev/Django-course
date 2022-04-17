893c71e831a88b53f93780e9dfdc5fe3578f3ce1
v.revenko <RevenkoVM@Gmail.com>
2022-04-17 18:39:49 +0300

    add users tags

--- news/views.py
+++ news/views.py
@@ -3,39 +3,23 @@ from django.http import HttpResponse
 from .models import News, Category
 
 
-# Create your views here.
-
 
 def index(request):
     news = News.objects.order_by('-created_at')
 
-    categories = Category.objects.all()
-
     context = {
         'news': news,
-        'title': 'Список новостей',
-        'categories': categories
-    }
+        'title': 'Список новостей'
+     }
 
     return render(request, 'news/index.html', context)
-
-    # print(dir(request))
-    # print(request.META)
-    # news = News.objects.all()
-    # res = '<H1>Список новостей</H1>'
-    # for item in news:
-    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-    # return HttpResponse(res)
-    # return HttpResponse('hello world!')
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category
     }
 
